0870cb84fb6e1bf2ee3c454d6aad80a4795aa220
samuel-barrett <90578414+samuel-barrett@users.noreply.github.com>
2022-03-27 18:35:31 -0700

    Update linked_list.c

--- src/linked_list.c
+++ src/linked_list.c
@@ -37,7 +37,7 @@ dd_task_node_t *get_head(dd_task_list_t *list) {
 
 /**
  * @brief get the next node in the linked list
- * 
+ *
  * @param node (dd_task_node_t *) [IN] Pointer to the node in the linked list
  * @return dd_task_node_t * Pointer to the next node in the linked list
  */
@@ -61,7 +61,7 @@ void push(dd_task_list_t *list, dd_task_t task) {
 
     while(curr != NULL && curr->task.absolute_deadline < task.absolute_deadline) {
         prev = curr;
-        curr = get_next(curr);
+        curr = curr->next;
     }
     if (prev == NULL) { //Insert at the head
         new_node->next = list->head;
@@ -86,7 +86,7 @@ dd_task_t *get_task(dd_task_list_t *list, uint32_t task_id) {
         if (curr->task.task_id == task_id) {
             return &curr->task;
         }
-        curr = get_next(curr);
+        curr = curr->next;
     }
     return NULL;
 }
@@ -115,7 +115,7 @@ TaskHandle_t remove_task(dd_task_list_t *list, uint32_t task_id) {
             return t_handle;
         }
         prev = curr;
-        curr = get_next(curr);
+        curr = curr->next;
     }
     return NULL;
 }
@@ -138,7 +138,7 @@ void free_list(dd_task_list_t *list) {
 }
 
 /**
- * @brief print items in the linked list from first to last. Prints 
+ * @brief print items in the linked list from first to last. Prints
  *       the task id, the task type, the task release time, and the
  *       task deadline
  *
@@ -152,210 +152,11 @@ void print_list(dd_task_list_t *list, char * list_name) {
     while (curr != NULL) {
         printf("ID:%d\t\t", curr->task.task_id);
         printf("Type: %s\t\t", curr->task.type == PERIODIC ? "PERIODIC" : "APERIODIC");
-        printf("Release:%d\t", curr->task.release_time);
+        printf("Release:%d\t\n", curr->task.release_time);
         printf("Deadline:%d\t", curr->task.absolute_deadline);
-        printf("Completion Time: %d\n", curr->task.completion_time);
+        printf("Completion Time: %d\t", curr->task.completion_time);
+        printf("User Task ID %d\n\n", curr->task.user_task_id);
         curr = get_next(curr);
     }
     printf("-----------------------------------------------------\n\n");
 }
-
-/*
-void assert(int condition, char *message) {
-    if (!condition) {
-        printf("%s\n", message);
-        exit(1);
-    }
-}
-
-
-//Test 1: Initialize the linked list
-void test1(void) {
-    printf("\n\nTest 1: Initialize the linked list\n");
-    dd_task_list_t list;
-    init_task_list(&list);
-    assert(list.head == NULL, "Head should be NULL");
-    assert(list.size == 0, "Size should be 0");
-    print_list(&list);
-    free_list(&list);
-}
-
-//Test 2: Push a task into the linked list
-void test2(void) {
-    printf("\n\nTest 2: Push a task into the linked list\n");
-    dd_task_list_t list;
-    init_task_list(&list);
-    dd_task_t task;
-    task.task_id = 1;
-    task.type = PERIODIC;
-    task.absolute_deadline = 2;
-    task.completion_time = 3;
-    task.release_time = 4;
-
-    //Push the task into the linked list
-    push(&list, task);
-
-    //Check if the task is in the linked list
-    assert(list.head != NULL, "Task not in the linked list");
-    assert(list.head->task.task_id == 1, "Task id is not correct");
-    print_list(&list);
-    free_list(&list);
-}
-
-
-
-//Test 3: Push 10 tasks into the linked list
-void test3(void) {
-    printf("\n\nTest 3: Push multiple tasks into the linked list\n");
-    dd_task_list_t list;
-    init_task_list(&list);
-
-    //Create array of tasks
-    dd_task_t tasks[10];
-
-    //Push 10 tasks into the linked list
-    for (int i = 0; i < 10; i++) {
-        tasks[i].task_id = i;
-        tasks[i].type = PERIODIC;
-        //Randomly generate the deadline
-        tasks[i].absolute_deadline = rand() % 30;
-        tasks[i].completion_time = i;
-        tasks[i].release_time = i;
-        push(&list, tasks[i]);
-    }
-
-    //Check if the tasks are in the linked list
-    assert(list.head != NULL, "list head is NULL");
-    assert(list.size == 10, "list size is not 10");
-    //assert(list.head->task_id == 9, "task id is not 9");
-    print_list(&list);
-    free_list(&list);
-}
-
-//Test 4: Pop a task from the linked list by id
-void test4(void) {
-    printf("\n\nTest 4: Pop a task from the linked list\n");
-    dd_task_list_t list;
-    init_task_list(&list);
-
-    //Create array of tasks
-    dd_task_t tasks[10];
-
-    //Push 10 tasks into the linked list
-    for (int i = 0; i < 10; i++) {
-        tasks[i].task_id = i;
-        tasks[i].type = PERIODIC;
-        //Randomly generate the deadline
-        tasks[i].absolute_deadline = rand() % 30;
-        tasks[i].completion_time = i;
-        tasks[i].release_time = i;
-        push(&list, tasks[i]);
-    }
-    print_list(&list);
-    
-    //Remove the task with id 5
-    printf("Removing task with id 5\n");
-    TaskHandle_t t_handle = remove_task(&list, 5);
-    printf("List size: %d\n", list.size);
-    printf("Task handle: %d\n", t_handle);
-    
-    //Check if the task is in the linked list
-    assert(list.size == 9, "Size is not 9");
-    print_list(&list);
-
-
-    //Iterate through the linked list and check if the task is still there
-    dd_task_node_t *curr = list.head;
-    while (curr != NULL) {
-        assert(curr->task.task_id != 5, "Task id is not 5");
-        curr = get_next(curr);
-    }
-
-    //Get the id of the first element in the linked list
-    int head_id = list.head->task.task_id;
-
-    //Remove the first element in the linked list
-    printf("Removing task with id %d\n", head_id);
-    t_handle = remove_task(&list, head_id);
-    printf("Task handle: %d\n", t_handle);
-    assert(list.size == 8, "Size is not 8");
-    print_list(&list);
-
-    //Iterate through the linked list and check if the task is still there
-    curr = list.head;
-    while (curr != NULL) {
-        assert(curr->task.task_id != head_id, "Task id is wrong");
-        curr = get_next(curr);
-    }
-
-    //Get the id of the last element in the linked list
-    int tail_id = -1;
-    curr = list.head;
-    for (int i = 0; i < list.size; i++) {
-        tail_id = curr->task.task_id;
-        curr = get_next(curr);
-    }
-    assert(tail_id != -1, "Didn't find the tail id");
-
-    //Remove the last element in the linked list
-    printf("Removing tail task %d\n", tail_id);
-    t_handle = remove_task(&list, tail_id);
-    printf("Task handle: %d\n", t_handle);
-    assert(list.size == 7, "Size is not 7");
-    print_list(&list);
-
-    //Iterate through the linked list and check if the task is still there
-    curr = list.head;
-    while (curr != NULL) {
-        assert(curr->task.task_id != tail_id, "Task id is wrong");
-        curr = get_next(curr);
-    }
-    free_list(&list);
-}
-
-//Test get_task functionaity
-void test5(void) {
-    printf("\n\nTest 5: Get a task from the linked list\n");
-    dd_task_list_t list;
-    init_task_list(&list);
-
-    //Create array of tasks
-    dd_task_t tasks[10];
-    
-    //Push 10 tasks into the linked list
-    for (int i = 0; i < 10; i++) {
-        tasks[i].task_id = i;
-        tasks[i].type = PERIODIC;
-        //Randomly generate the deadline
-        tasks[i].absolute_deadline = rand() % 30;
-        tasks[i].completion_time = i;
-        tasks[i].release_time = i;
-        push(&list, tasks[i]);
-    }
-    print_list(&list);
-    //Get the task with id 5
-    printf("Getting task with id 5\n");
-    dd_task_t *task = get_task(&list, 5);
-
-    printf("Task id: %d\tTask type: %d\tTask deadline: %d\tTask completion time: %d\tTask release time: %d\n", task->task_id, task->type, task->absolute_deadline, task->completion_time, task->release_time);
-
-    //Check if the task is correct
-    assert(task->task_id == 5, "Task id is not 5");
-    assert(task->type == PERIODIC, "Task type is not PERIODIC");
-    assert(task->completion_time == 5, "Task completion time is not 5");
-    assert(task->release_time == 5, "Task release time is not 5");
-
-    free_list(&list);
-}
-
-
-int main(int argc, char *argv[]) {
-    srand(time(NULL));
-    test1();
-    test2();
-    test3();
-    test4();
-    test5();
-    return 0;
-}
-*/
