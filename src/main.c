cb3dcaec14531ad7beef12506e48bf1c952b8af1
samuel-barrett <90578414+samuel-barrett@users.noreply.github.com>
2022-03-28 19:14:14 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -28,29 +28,49 @@
 
 /*-----------------------------------------------------------*/
 #define mainQUEUE_LENGTH 100
-#define TASK1_PERIOD 500
-#define TASK2_PERIOD 500
-#define TASK3_PERIOD 750
-#define TASK1_EXEC_TIME 95
-#define TASK2_EXEC_TIME 150
-#define TASK3_EXEC_TIME 250
 
-#define amber  	0
+//#define TEST_BENCH_1 1
+//#define TEST_BENCH_2 2
+#define TEST_BENCH_3 3
+
+#ifdef TEST_BENCH_1
+	#define TASK1_PERIOD 500
+	#define TASK2_PERIOD 500
+	#define TASK3_PERIOD 750
+	#define TASK1_EXEC_TIME 95
+	#define TASK2_EXEC_TIME 150
+	#define TASK3_EXEC_TIME 250
+#endif
+
+#ifdef TEST_BENCH_2
+	#define TASK1_PERIOD 250
+	#define TASK2_PERIOD 500
+	#define TASK3_PERIOD 750
+	#define TASK1_EXEC_TIME 95
+	#define TASK2_EXEC_TIME 150
+	#define TASK3_EXEC_TIME 250
+#endif
+
+#ifdef TEST_BENCH_3
+	#define TASK1_PERIOD 500
+	#define TASK2_PERIOD 500
+	#define TASK3_PERIOD 500
+	#define TASK1_EXEC_TIME 100
+	#define TASK2_EXEC_TIME 200
+	#define TASK3_EXEC_TIME 200
+#endif
+
 #define amber_led	LED3
-#define green  	1
 #define green_led	LED4
-#define red  	2
 #define red_led		LED5
 
-
 #define DDS_PRIORITY				( tskIDLE_PRIORITY + 3 )
-#define TIMER_PRIORITIES			( tskIDLE_PRIORITY + 1 )
 
 #define USER_ACTIVE_TASK_PRIORITY	( tskIDLE_PRIORITY + 2 )
 #define USER_IDLE_TASK_PRIORITY		( tskIDLE_PRIORITY + 0)
 
-#define MONITOR_ACTIVE_PRIORITY		( tskIDLE_PRIORITY + 4 )
-#define MONITOR_IDLE_PRIORITY		( tskIDLE_PRIORITY )
+#define MONITOR_ACTIVE_PRIORITY		( tskIDLE_PRIORITY + 5 )
+#define MONITOR_IDLE_PRIORITY		( tskIDLE_PRIORITY + 0 )
 
 
 #define pdTICKS_TO_MS( xTicks ) ( ( uint32_t ) ( ( ( uint32_t ) ( xTicks ) * ( uint32_t ) 1000 )  / ( uint32_t ) configTICK_RATE_HZ ) )
@@ -97,18 +117,20 @@ xQueueHandle xQueue_active_task_list = 0;
 xTimerHandle xTimer_task1 = 0;
 xTimerHandle xTimer_task2 = 0;
 xTimerHandle xTimer_task3 = 0;
+xSemaphoreHandle monitor_task_lock = 0;
 
 
 int main(void){
 	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	prvSetupHardware();
 
 	STM_EVAL_LEDInit(amber_led);
+	STM_EVAL_LEDInit(red_led);
+	STM_EVAL_LEDInit(green_led);
 
 	//Create queues
 	xQueue_new_dd_task = xQueueCreate(mainQUEUE_LENGTH, sizeof(dd_task_t));
-	xQueue_completed_dd_task = xQueueCreate(mainQUEUE_LENGTH, sizeof(uint32_t)); //TODO change to uint32_t
+	xQueue_completed_dd_task = xQueueCreate(mainQUEUE_LENGTH, sizeof(uint32_t));
 	xQueue_request_overdue_task_list = xQueueCreate(mainQUEUE_LENGTH, sizeof(dd_task_list_t));
 	xQueue_request_completed_task_list = xQueueCreate(mainQUEUE_LENGTH, sizeof(dd_task_list_t));
 	xQueue_request_active_task_list = xQueueCreate(mainQUEUE_LENGTH, sizeof(dd_task_list_t));
@@ -124,16 +146,19 @@ int main(void){
 	vQueueAddToRegistry(xQueue_request_overdue_task_list, "RequestOverdueTaskListQueue");
 	vQueueAddToRegistry(xQueue_request_completed_task_list, "RequestCompletedTaskListQueue");
 
-	xTaskCreate(DDS_Task, "DDS_Task", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+	xTaskCreate(DDS_Task, "DDS_Task", configMINIMAL_STACK_SIZE, NULL, DDS_PRIORITY, NULL);
 
 	//Create timers
 	xTimer_task1 = xTimerCreate("Task 1 Timer", 1, pdTRUE, NULL, Task_Generator_Task);
 	xTimer_task2 = xTimerCreate("Task 2 Timer", 1, pdTRUE, NULL, Task_Generator_Task);
 	xTimer_task3 = xTimerCreate("Task 3 Timer", 1, pdTRUE, NULL, Task_Generator_Task);
-
+	// Start timers
 	xTimerStart(xTimer_task1, 0);
 	xTimerStart(xTimer_task2, 0);
 	xTimerStart(xTimer_task3, 0);
+
+	monitor_task_lock = xSemaphoreCreateBinary();
+	xSemaphoreGive(monitor_task_lock);
 
 	vTaskStartScheduler();
 	
@@ -151,34 +176,27 @@ void update_priorities(dd_task_list_t *active_task_list) {
 	if(active_task_list->size == 0) {
 		return;
 	}
-
 	dd_task_node_t *head = get_head(active_task_list);
 	vTaskPrioritySet(head->task.t_handle, USER_ACTIVE_TASK_PRIORITY);
-
-	//printf("Setting user task %u to priority %u\n", head->task.user_task_id, USER_ACTIVE_TASK_PRIORITY);
-
-
 	if(active_task_list->size == 1) {
 		return;
 	}
-
 	dd_task_node_t *curr = get_next(head);
-
 	do {
 		//Set other priorities to idle priority
 		vTaskPrioritySet(curr->task.t_handle, USER_IDLE_TASK_PRIORITY);
-
-		//printf("Setting user task %u to priority %u\n", curr->task.user_task_id, USER_IDLE_TASK_PRIORITY);
-
 		curr = get_next(curr);
 	} while(curr != NULL);
 }
 
 
 void upgrade_monitor_task_priority(TaskHandle_t monitor_t_handle){
-	vTaskPrioritySet(monitor_t_handle, 4);
+	taskENTER_CRITICAL();
+	vTaskPrioritySet(monitor_t_handle, MONITOR_ACTIVE_PRIORITY);
+	vTaskResume(monitor_t_handle);
+	taskYIELD();
+	taskEXIT_CRITICAL();
 }
-
 
 /**
  * @brief This task is responsible for creating user defined tasks,
@@ -204,20 +222,19 @@ static void DDS_Task( void *pvParameters )
 	dd_task_list_t tmp_buffer;
 	init_task_list(&tmp_buffer);
 	uint32_t task_id_cnt = 0;
-	TaskHandle_t monitor_t_handle;
+	TaskHandle_t monitor_t_handle = NULL;
 
-	xTaskCreate(Monitor_Task, "Monitor_Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, &monitor_t_handle);
 	for(;;){
 		if(xQueueReceive(xQueue_new_dd_task, &new_task, 0)){ //New task received
+			// Set unique task ID
 			new_task.task_id = task_id_cnt;
 			// Add new task to active task list and sort by deadline
 			push(&active_task_list, new_task);
 			dd_task_t *task_list_task = get_task(&active_task_list, new_task.task_id);
 			// Create new task in FreeRTOS
 			if(task_list_task->user_task_id == 1) {
-				//Create copy of task
-					xTaskCreate(User_Defined_Task1, "User_Defined_Task1",
-				configMINIMAL_STACK_SIZE, task_list_task, USER_IDLE_TASK_PRIORITY, &(task_list_task->t_handle));
+				xTaskCreate(User_Defined_Task1, "User_Defined_Task1",
+								configMINIMAL_STACK_SIZE, task_list_task, USER_IDLE_TASK_PRIORITY, &(task_list_task->t_handle));
 			} else if(task_list_task->user_task_id == 2) {
 				xTaskCreate(User_Defined_Task2, "User_Defined_Task2",
 								configMINIMAL_STACK_SIZE, task_list_task, USER_IDLE_TASK_PRIORITY , &(task_list_task->t_handle));
@@ -225,49 +242,54 @@ static void DDS_Task( void *pvParameters )
 				xTaskCreate(User_Defined_Task3, "User_Defined_Task3",
 								configMINIMAL_STACK_SIZE, task_list_task, USER_IDLE_TASK_PRIORITY, &(task_list_task->t_handle));
 			}else{
-				//printf("Invalid task id\n");
+				// Aperiodic task
 			}
 			// Add release time to dd_task
-			task_list_task->release_time = xTaskGetTickCount();
-
-			print_list(&active_task_list, "Added new task; Active");
+			task_list_task->release_time = pdMS_TO_TICKS(xTaskGetTickCount());
+			// Update task priorities in FreeRTOS to reflect EDF sorting
 			update_priorities(&active_task_list);
 
 			task_id_cnt++;
 		}
 		if(xQueueReceive(xQueue_completed_dd_task, &completed_task_id, 0)){ //Task completed
 			dd_task_t *completed_task = get_task(&active_task_list, completed_task_id);
-
 			// Remove task from active task list
 			remove_task(&active_task_list, completed_task_id);
-			print_list(&active_task_list, "Completed new task; Active");
+			// Add task to completed list
+			push(&completed_task_list, *completed_task);
+			// Add completion time to dd_task struct
+			completed_task->completion_time = pdTICKS_TO_MS(xTaskGetTickCount());
+			// Update task priorities in FreeRTOS to reflect EDF sorting
 			update_priorities(&active_task_list);
 
-			completed_task->completion_time = xTaskGetTickCount();
-			// Add task to completed list
-
-			push(&completed_task_list, *completed_task);
-
-
-			//upgrade_monitor_task_priority(monitor_t_handle);
+			if(xSemaphoreTake(monitor_task_lock, 0)){
+				if(!monitor_t_handle){
+					xTaskCreate(Monitor_Task, "Monitor_Task", configMINIMAL_STACK_SIZE, NULL, MONITOR_IDLE_PRIORITY, &monitor_t_handle);
+				}
+				upgrade_monitor_task_priority(monitor_t_handle);
+			}
 		}
 		//Check if any tasks are overdue
 		if(active_task_list.size > 0){
 			dd_task_node_t *head = get_head(&active_task_list);
-			if(xTaskGetTickCount() > head->task.absolute_deadline){
+			if(xTaskGetTickCount() > head->task.absolute_deadline){ // Task is overdue
 				//Add task to overdue list
 				push(&overdue_task_list, head->task);
-
 				//Remove task from active task list
-				remove_task(&active_task_list, head->task.task_id);
-
-				//Delete task from FreeRTOS
-				vTaskDelete(head->task.t_handle);
-
+				TaskHandle_t overdue_t_handle = remove_task(&active_task_list, head->task.task_id);
 				update_priorities(&active_task_list);
+				//Delete task from FreeRTOS
+				vTaskDelete(overdue_t_handle);
+				STM_EVAL_LEDOff(amber_led);
+				STM_EVAL_LEDOff(red_led);
+				STM_EVAL_LEDOff(green_led);
 
-				//upgrade_monitor_task_priority(monitor_t_handle);
-				print_list(&active_task_list, "Overdue; Active");
+				if(xSemaphoreTake(monitor_task_lock, 0)){
+					if(!monitor_t_handle){
+						xTaskCreate(Monitor_Task, "Monitor_Task", configMINIMAL_STACK_SIZE, NULL, MONITOR_IDLE_PRIORITY, &monitor_t_handle);
+					}
+					upgrade_monitor_task_priority(monitor_t_handle);
+				}
 			}
 		}
 		if(xQueueReceive(xQueue_request_active_task_list, &tmp_buffer, 0)){ //Active task list requested
@@ -279,7 +301,7 @@ static void DDS_Task( void *pvParameters )
 		if(xQueueReceive(xQueue_request_overdue_task_list, &tmp_buffer, 0)){ //Overdue task list requested
 			xQueueSend(xQueue_overdue_task_list, &overdue_task_list, 500);
 		}
-		vTaskDelay(20);
+		vTaskDelay(1);
 	}
 }
 
@@ -305,8 +327,8 @@ dd_task_list_t get_active_dd_task_list(void)
 {
 	dd_task_list_t active_task_list;
 	init_task_list(&active_task_list);
-	if(xQueueSend(xQueue_request_active_task_list, &active_task_list, 1000) == pdTRUE);
-	if(xQueueReceive(xQueue_active_task_list, &active_task_list, 1000) == pdTRUE);
+	xQueueSend(xQueue_request_active_task_list, &active_task_list, 1000);
+	xQueueReceive(xQueue_active_task_list, &active_task_list, 1000);
 	return active_task_list;
 }
 
@@ -346,8 +368,9 @@ dd_task_list_t get_overdue_dd_task_list(void) {
  * @param pvParameters (void *) [in] Unused, should be NULL.
  * @return (static void) Does not return.
  */
-static void Monitor_Task( void *pvParameters )
+static void Monitor_Task(void *pvParameters)
 {
+
 	dd_task_list_t active_task_list;
 	init_task_list(&active_task_list);
 	dd_task_list_t completed_task_list;
@@ -355,15 +378,23 @@ static void Monitor_Task( void *pvParameters )
 	dd_task_list_t overdue_task_list;
 	init_task_list(&overdue_task_list);
 	for(;;){
+		// Request task information from DDS Task
 		active_task_list = get_active_dd_task_list();
 		completed_task_list = get_completed_dd_task_list();
 		overdue_task_list = get_overdue_dd_task_list();
-
+		taskENTER_CRITICAL();
+		// Print task information
+		printf("Monitor Task | Current Time: %u\n", (uint16_t)pdTICKS_TO_MS(xTaskGetTickCount()));
 		print_list(&active_task_list, "Active");
 		print_list(&completed_task_list, "Completed");
 		print_list(&overdue_task_list, "Overdue");
+		printf("-----------------------------\n");
 
-		vTaskPrioritySet(xTaskGetCurrentTaskHandle(), tskIDLE_PRIORITY);
+		xSemaphoreGive(monitor_task_lock);
+		// Downgrade priority of monitor task to IDLE
+		vTaskPrioritySet(xTaskGetCurrentTaskHandle(), MONITOR_IDLE_PRIORITY);
+		vTaskSuspend(NULL);
+		taskEXIT_CRITICAL();
 	}
 }
 
@@ -383,6 +414,7 @@ void release_dd_task(
 ){
 	dd_task_t new_task;
 	new_task.type = type;
+	new_task.completion_time = 0;
 	new_task.user_task_id = user_task_id;
 	new_task.absolute_deadline = absolute_deadline;
 	xQueueSend(xQueue_new_dd_task,&new_task,1000);
@@ -400,21 +432,20 @@ void Task_Generator_Task( TimerHandle_t xTimer )
 {
 	enum task_type t;
 
-	if(xTimer == xTimer_task1){ //TODO: task_id & how to pass in exec. time? (maybe add to dd_task?)
+	if(xTimer == xTimer_task1){
 		t = PERIODIC;
 		release_dd_task(t, 1, xTaskGetTickCount()+pdMS_TO_TICKS(TASK1_PERIOD));
-		//printf("task 1 deadline: %u\n", xTaskGetTickCount()+pdMS_TO_TICKS(TASK1_PERIOD));
 		xTimerChangePeriod(xTimer, pdMS_TO_TICKS(TASK1_PERIOD), 1000);
 	} else if(xTimer == xTimer_task2){
 		t = PERIODIC;
 		release_dd_task(t, 2, xTaskGetTickCount()+pdMS_TO_TICKS(TASK2_PERIOD));
-		//printf("task 2 deadline: %u\n", xTaskGetTickCount()+pdMS_TO_TICKS(TASK2_PERIOD));
 		xTimerChangePeriod(xTimer, pdMS_TO_TICKS(TASK2_PERIOD), 1000);
 	} else if(xTimer == xTimer_task3){
 		t = PERIODIC;
 		release_dd_task(t, 3, xTaskGetTickCount()+pdMS_TO_TICKS(TASK3_PERIOD));
-		//printf("task 3 deadline: %u\n", xTaskGetTickCount()+pdMS_TO_TICKS(TASK3_PERIOD));
 		xTimerChangePeriod(xTimer, pdMS_TO_TICKS(TASK3_PERIOD), 1000);
+	} else{
+		// Aperiodic Implementation
 	}
 
 }
@@ -437,8 +468,6 @@ static void User_Defined_Task1( void * pvParameters)
 
 	uint32_t ticks = pdMS_TO_TICKS(TASK1_EXEC_TIME);
 
-	//printf("Starting user t1:\n\tTask ID: %u\tExecution Time: %u\tUserTaskID: %u\n", task->task_id, ticks, task->user_task_id);
-
 	while(ticks--){
 		prev_ticks = xTaskGetTickCount();
 		do {
@@ -447,8 +476,6 @@ static void User_Defined_Task1( void * pvParameters)
 
 	}
 	STM_EVAL_LEDOff(amber_led);
-
-	//printf("Exiting user t1\n\tTask ID: %u\tUserTaskID: %u\n", task->task_id, task->user_task_id);
 
 	complete_dd_task(task->task_id);
 	vTaskDelete(xTaskGetCurrentTaskHandle());
@@ -470,9 +497,6 @@ static void User_Defined_Task2( void * pvParameters)
 
 	uint32_t ticks = pdMS_TO_TICKS(TASK2_EXEC_TIME);
 
-	//printf("Starting user t2\n\tTask ID: %u\tExecution Time: %u\tUserTaskID: %u\n", task->task_id, ticks, task->user_task_id);
-	//fflush(stdout);
-
 	while(ticks--){
 		prev_ticks = xTaskGetTickCount();
 		do {
@@ -481,8 +505,6 @@ static void User_Defined_Task2( void * pvParameters)
 
 	}
 	STM_EVAL_LEDOff(green_led);
-
-	//printf("Exiting user t2\n\tTask ID: %u\tUserTaskID: %u\n", task->task_id, task->user_task_id);
 
 	complete_dd_task(task->task_id);
 	vTaskDelete(xTaskGetCurrentTaskHandle());
@@ -504,9 +526,6 @@ static void User_Defined_Task3( void * pvParameters)
 
 	uint32_t ticks = pdMS_TO_TICKS(TASK3_EXEC_TIME);
 
-	//printf("Starting user t3\n\tTask ID: %u\tExecution Time: %u\tUserTaskID: %u\n", task->task_id, ticks, task->user_task_id);
-	//fflush(stdout);
-
 	while(ticks--){
 		prev_ticks = xTaskGetTickCount();
 		do {
@@ -515,8 +534,6 @@ static void User_Defined_Task3( void * pvParameters)
 
 	}
 	STM_EVAL_LEDOff(red_led);
-
-	//printf("Exiting user t3\n\tTask ID: %u\tUserTaskID: %u\n", task->task_id, task->user_task_id);
 
 	complete_dd_task(task->task_id);
 	vTaskDelete(xTaskGetCurrentTaskHandle());
